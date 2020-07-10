f9b4f509cc0f409268c0684e16ffe0f18cda4cde
xiongjunlu <xiongjunlu@jd.com>
2020-07-10 19:58:07 +0800

    grpc

--- client.js
+++ client.js
@@ -1,13 +1,10 @@
 const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
- 
 const PROTO_PATH = __dirname + '/protos/test.proto'
 const testProto = protoLoader.loadSync(PROTO_PATH)
 
-var testService = grpc.loadPackageDefinition(testProto).testPackage;
- 
+let testService = grpc.loadPackageDefinition(testProto).testPackage;
 const client = new testService.testService('0.0.0.0:3001', grpc.credentials.createInsecure());
- 
-client.ping({param: 'param'}, function(err, response) {
+client.ping({param: 'param'}, (err, response) => {
     console.log('ping -> :', response.message);
 });
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
- 
 const PROTO_PATH = __dirname + '/protos/test.proto' // proto文件地址
 const testProto = protoLoader.loadSync(PROTO_PATH) // 加载proto文件
 const packageObject = grpc.loadPackageDefinition(testProto, {
@@ -11,7 +10,6 @@ const packageObject = grpc.loadPackageDefinition(testProto, {
   oneofs: true
 });
 
-
 let ping = (call, callback) => {
   console.log(call)
   callback(null, {message: 'Pong'})
@@ -19,7 +17,6 @@ let ping = (call, callback) => {
  
 const server = new grpc.Server();
 server.addService(packageObject.testPackage.testService.service, {ping: ping})
-
 server.bindAsync('0.0.0.0:3001', grpc.ServerCredentials.createInsecure(), function (result) {
   server.start()
 })
